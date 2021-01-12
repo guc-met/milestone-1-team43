7e621504fb41659019d7d531af942decbdbfa7df
Razan <razanelbadawy@gmail.com>
2021-01-12 14:41:52 +0200

    my commit

--- myapp/src/components/Register.js
+++ myapp/src/components/Register.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 import bg from '../images/1.png'
+import Button from 'react-bootstrap/Button'
+
 
 
 import { useHistory } from "react-router";
@@ -10,10 +12,10 @@ import { useHistory } from "react-router";
 export default function Register() {
     const history = useHistory()
     
-    // const redirect = () => {
-    //     history.push('/Profile')
-    //     console.log('true')
-    //   }
+    const redirect = () => {
+        history.push('/Profile')
+        console.log('true')
+      }
       const redirect1 = () => {
         history.push('/')
         console.log('true')
@@ -79,6 +81,53 @@ export default function Register() {
          
                       
     </td>
+
+    <td>
+    <Form.Group
+                        style={{ marginTop:'1vw' }}
+                      >
+                        <Form.Label>
+                          <text
+                            style={{
+                              fontWeight: 'bold',
+                              textAlign: 'left',
+                              fontSize: '1vw',
+                              color:"#33b889",
+                              marginLeft: '13vw',
+                              marginRight:"1vw",fontWeight:'bold',fontFamily:'Georgia, serif'
+                            }}
+                          >
+                           Name
+                          </text>
+                        </Form.Label>
+                        <Form.Control
+                          required
+                          style={{
+                            fontSize: '0.8vw',
+                            width: '20vw',
+                            height: '2.5vw',
+                            borderColor: 'white',
+                            borderRadius: '0.5vw',
+                            borderWidth: '0.1vw',
+                            boxShadow: 'inset 1px 1px 4px rgba(0, 0, 0, 0.25)',
+                            outline:'none',fontFamily:'Georgia, serif'
+
+                          }}
+                          placeholder="Enter Staff's Name"
+                          type='email'
+                          
+
+                        //   value=''
+                        //   onChange=''
+                        />
+                      </Form.Group>
+                      
+         
+                      
+    </td>
+
+
+
     <td>
     <Form.Group
                         style={{ marginTop:'1vw' }}
@@ -247,6 +296,28 @@ export default function Register() {
 </tr>
 
 </table> 
+
+
+
+<tr>
+                          <Button style={{  height: '2vw',
+                  display: 'flex',
+                  alignSelf: 'center',
+                  marginRight: 'auto',
+                  marginLeft: '55.5vw',
+                  marginTop:'2vw',
+                  fontSize: '1.2vw',
+                  justifyContent: 'center',
+                  color:'white',
+                  alignItems: 'center',marginBottom:'1vw',
+                  backgroundColor:'#33b889',border:'none',borderRadius: '0.5vw', outline:'none',fontWeight:'bold',fontFamily:'Georgia, serif'}} onClick={() => {this.refs.video.play()}}> Submit </Button>
+                          </tr>
+
+                    <video controls ref="welcomeVid">
+                            <source src="VIDEO SOURCE" type="welcomeVid/mp4" />
+                    </video>
+
+
         </div>
     )
 }
